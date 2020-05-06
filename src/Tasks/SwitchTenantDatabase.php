26cf7770e06e84565027f10904965310766825ff
freek <freek@spatie.be>
2020-05-06 15:32:57 +0200

    wip

--- src/Tasks/SwitchTenantDatabase.php
+++ src/Tasks/SwitchTenantDatabase.php
@@ -3,14 +3,21 @@
 namespace Spatie\Multitenancy\Tasks;
 
 use Illuminate\Support\Facades\DB;
+use Spatie\Multitenancy\Exceptions\InvalidConfiguration;
 use Spatie\Multitenancy\Models\Tenant;
 
 class SwitchTenantDatabase implements MakeTenantCurrentTask
 {
     public function makeCurrent(Tenant $tenant): void
     {
+        $tenantConnectionName = config('multitenancy.tenant_database_connection_name');
+
+        if (is_null(config("database.connections.{$tenantConnectionName}"))) {
+            throw InvalidConfiguration::tenantConnectionDoesNotExist();
+        }
+
         config([
-            'database.connections.tenant.database' => $tenant->getDatabaseName(),
+            "database.connections.{$tenantConnectionName}.database" => $tenant->getDatabaseName(),
         ]);
 
         DB::purge('tenant');
