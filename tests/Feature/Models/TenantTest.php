3734d2dfb7ca37652bbb78d36cbb5d83c85f2824
freek <freek@spatie.be>
2020-05-08 09:41:11 +0200

    wip

--- tests/Feature/Models/TenantTest.php
+++ tests/Feature/Models/TenantTest.php
@@ -4,6 +4,8 @@ namespace Spatie\Multitenancy\Tests\Feature\Models;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Spatie\Multitenancy\Events\ForgettingCurrentTenantEvent;
+use Spatie\Multitenancy\Events\ForgotCurrentTenantEvent;
 use Spatie\Multitenancy\Events\MadeTenantCurrentEvent;
 use Spatie\Multitenancy\Events\MakingTenantCurrentEvent;
 use Spatie\Multitenancy\Models\Tenant;
@@ -59,6 +61,34 @@ class TenantTest extends TestCase
     }
 
     /** @test */
+    public function it_can_forget_the_current_tenant()
+    {
+        $containerKey = config('multitenancy.current_tenant_container_key');
+
+        $this->tenant->makeCurrent();
+        $this->assertEquals($this->tenant->id, Tenant::current()->id);
+        $this->assertTrue(app()->has($containerKey));
+
+        Tenant::forgetCurrent();
+        $this->assertNull(Tenant::current());
+        $this->assertFalse(app()->has($containerKey));
+    }
+
+    /** @test */
+    public function it_can_check_if_a_current_tenant_has_been_set()
+    {
+        $this->assertFalse(Tenant::checkCurrent());
+
+        $this->tenant->makeCurrent();
+
+        $this->assertTrue(Tenant::checkCurrent());
+
+        Tenant::forgetCurrent();
+
+        $this->assertFalse(Tenant::checkCurrent());
+    }
+
+    /** @test */
     public function it_will_fire_off_events_when_making_a_tenant_current()
     {
         Event::fake();
@@ -70,5 +100,32 @@ class TenantTest extends TestCase
 
         Event::assertDispatched(MakingTenantCurrentEvent::class);
         Event::assertDispatched(MadeTenantCurrentEvent::class);
+    }
+
+    /** @test */
+    public function it_will_fire_off_events_when_forgetting_the_current_tenant()
+    {
+        Event::fake();
+
+        $this->tenant->makeCurrent();
+
+        Event::assertNotDispatched(ForgettingCurrentTenantEvent::class);
+        Event::assertNotDispatched(ForgotCurrentTenantEvent::class);
+
+        Tenant::forgetCurrent();
+
+        Event::assertDispatched(ForgettingCurrentTenantEvent::class);
+        Event::assertDispatched(ForgotCurrentTenantEvent::class);
+    }
+
+    /** @test */
+    public function it_will_not_fire_off_events_when_forgetting_the_current_tenant_when_not_current_tenant_is_set()
+    {
+        Event::fake();
+
+        Tenant::forgetCurrent();
+
+        Event::assertNotDispatched(ForgettingCurrentTenantEvent::class);
+        Event::assertNotDispatched(ForgotCurrentTenantEvent::class);
     }
 }
