23200261a06312327dc2196c4ddbb5be15051ed0
freek <freek@spatie.be>
2020-05-08 21:08:10 +0200

    wip

--- tests/Feature/TestClasses/TestJob.php
+++ tests/Feature/TestClasses/TestJob.php
@@ -3,14 +3,11 @@
 namespace Spatie\Multitenancy\Tests\Feature\TestClasses;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Spatie\Multitenancy\Models\Tenant;
 use Spatie\Valuestore\Valuestore;
 
 class TestJob implements ShouldQueue
 {
-    use InteractsWithQueue;
-
     public Valuestore $valuestore;
 
     public function __construct(Valuestore $valuestore)
