4e3ab2d45fef4a3cf979fa5600a117f0db7fd53f
freekmurze <freekmurze@users.noreply.github.com>
2020-05-09 10:11:46 +0000

    Fix styling

--- tests/Feature/TenantAwareJobs/TestClasses/TenantAwareTestJob.php
+++ tests/Feature/TenantAwareJobs/TestClasses/TenantAwareTestJob.php
@@ -2,12 +2,8 @@
 
 namespace Spatie\Multitenancy\Tests\Feature\TenantAwareJobs\TestClasses;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Spatie\Multitenancy\Jobs\TenantAware;
-use Spatie\Multitenancy\Models\Tenant;
-use Spatie\Valuestore\Valuestore;
 
 class TenantAwareTestJob extends TestJob implements TenantAware
 {
-
 }
