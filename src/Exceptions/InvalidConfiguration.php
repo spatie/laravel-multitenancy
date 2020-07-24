0cde83d5ed64e38f408ff1a687b98e1bb91c09c3
Luca Longo <l.longo@ambita.it>
2020-07-24 11:54:37 +0200

    WIP

--- src/Exceptions/InvalidConfiguration.php
+++ src/Exceptions/InvalidConfiguration.php
@@ -15,7 +15,7 @@ class InvalidConfiguration extends Exception
     /** @test */
     public static function tenantConnectionIsEmptyOrEqualsToLandlordConnection(): self
     {
-        return new static("`SwitchTenantDatabaseTask` fails because `multitenancy.tenant_database_connection_name` is `null` or equals to `multitenancy.tenant_database_connection_name`.");
+        return new static("`SwitchTenantDatabaseTask` fails because `multitenancy.tenant_database_connection_name` is `null` or equals to `multitenancy.landlord_database_connection_name`.");
     }
 
     public static function invalidAction(string $actionName, string $configuredClass, string $actionClass): self
