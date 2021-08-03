dc428b3067cf07802a134147bfa4ce732f872843
Luca Longo <l.longo@ambita.it>
2021-08-03 19:35:27 +0200

    Set current tenant foreign key if empty

--- src/Models/Concerns/BelongsToTenant.php
+++ src/Models/Concerns/BelongsToTenant.php
@@ -4,7 +4,10 @@ namespace Spatie\Multitenancy\Models\Concerns;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Multitenancy\Exceptions\NoCurrentTenant;
 use Spatie\Multitenancy\Models\Concerns\Scopes\TenantScope;
+use Spatie\Multitenancy\Models\Tenant;
+use Spatie\Multitenancy\Multitenancy;
 
 /** @mixin Model */
 trait BelongsToTenant
@@ -12,6 +15,14 @@ trait BelongsToTenant
     public static function bootedBelongsToTenant(): void
     {
         static::addGlobalScope(new TenantScope());
+
+        static::creating(function (Model $model) {
+            $tenantForeignKey = app(Multitenancy::class)
+                ->getTenantModel()
+                ->getForeignKey();
+
+            $model->{$tenantForeignKey} ??= Tenant::current()?->getKey();
+        });
     }
 
     public function tenant(): BelongsTo
