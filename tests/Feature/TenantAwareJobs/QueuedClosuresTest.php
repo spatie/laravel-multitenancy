e49a09cf081df258772ee31d8bcd7925e535c1ab
Luca Longo <l.longo@ambita.it>
2023-09-26 12:18:03 +0200

    Update QueuedClosuresTest.php

--- tests/Feature/TenantAwareJobs/QueuedClosuresTest.php
+++ tests/Feature/TenantAwareJobs/QueuedClosuresTest.php
@@ -5,77 +5,75 @@ use Spatie\Valuestore\Valuestore;
 
 beforeEach(function () {
     config()->set('queue.default', 'database');
+    config()->set('multitenancy.queues_are_tenant_aware_by_default', false);
 
     $this->tenant = Tenant::factory()->create();
-    $this->valuestore = Valuestore::make(tempFile('tenantAware.json'))->flush();
 });
 
 it('succeeds with closure job when queues are tenant aware by default', function () {
+    $valuestore = Valuestore::make(tempFile('tenantAware.json'))->flush();
+
     config()->set('multitenancy.queues_are_tenant_aware_by_default', true);
 
-    $this->tenant->execute(function () {
-        dispatch(function () {
-            $valuestore = Valuestore::make(tempFile('tenantAware.json'));
+    $this->tenant->makeCurrent();
 
-            $tenant = Tenant::current();
+    dispatch(function () use(&$valuestore) {
+        $tenant = Tenant::current();
 
-            $valuestore->put('tenantId', $tenant?->getKey());
-            $valuestore->put('tenantName', $tenant?->name);
-        });
+        $valuestore->put('tenantId', $tenant?->getKey());
+        $valuestore->put('tenantName', $tenant?->name);
     });
 
-    expect($this->valuestore->get('tenantId'))->toBeNull()
-        ->and($this->valuestore->get('tenantName'))->toBeNull();
+    expect($valuestore->get('tenantId'))->toBeNull()
+        ->and($valuestore->get('tenantName'))->toBeNull();
 
     $this->artisan('queue:work --once');
 
-    expect($this->valuestore->get('tenantId'))->toBe($this->tenant->getKey())
-        ->and($this->valuestore->get('tenantName'))->toBe($this->tenant->name);
+    expect($valuestore->get('tenantId'))->toBe($this->tenant->getKey())
+        ->and($valuestore->get('tenantName'))->toBe($this->tenant->name);
 });
 
 it('fails with closure job when queues are not tenant aware by default', function () {
-    config()->set('multitenancy.queues_are_tenant_aware_by_default', false);
+    $valuestore = Valuestore::make(tempFile('tenantAware.json'))->flush();
 
-    $this->tenant->execute(function () {
-        dispatch(function () {
-            $valuestore = Valuestore::make(tempFile('tenantAware.json'));
+    $this->tenant->makeCurrent();
 
-            $tenant = Tenant::current();
+    dispatch(function () use (&$valuestore) {
+        $tenant = Tenant::current();
 
-            $valuestore->put('tenantId', $tenant?->getKey());
-            $valuestore->put('tenantName', $tenant?->name);
-        });
+        $valuestore->put('tenantId', $tenant?->getKey());
+        $valuestore->put('tenantName', $tenant?->name);
     });
 
     $this->artisan('queue:work --once');
 
-    expect($this->valuestore->get('tenantId'))->toBeNull()
-        ->and($this->valuestore->get('tenantName'))->toBeNull();
+    expect($valuestore->get('tenantId'))->toBeNull()
+        ->and($valuestore->get('tenantName'))->toBeNull();
 });
 
 it('succeeds with closure job when a tenant is specified', function () {
-    config()->set('multitenancy.queues_are_tenant_aware_by_default', false);
+    $valuestore = Valuestore::make(tempFile('tenantAware.json'))->flush();
 
-    $this->tenant->execute(function (Tenant $currentTenant) {
-        dispatch(function () use ($currentTenant) {
-            $valuestore = Valuestore::make(tempFile('tenantAware.json'));
+    $currentTenant = $this->tenant;
 
-            $currentTenant->makeCurrent();
+    dispatch(function () use (&$valuestore, $currentTenant) {
+        $valuestore = Valuestore::make(tempFile('tenantAware.json'));
 
-            $tenant = Tenant::current();
+        $currentTenant->makeCurrent();
 
-            $valuestore->put('tenantId', $tenant?->getKey());
-            $valuestore->put('tenantName', $tenant?->name);
+        $tenant = Tenant::current();
 
-            $currentTenant->forget();
-        });
+        $valuestore->put('tenantId', $tenant?->getKey());
+        $valuestore->put('tenantName', $tenant?->name);
+
+        $currentTenant->forget();
     });
 
-    expect($this->valuestore->get('tenantId'))->toBeNull()
-        ->and($this->valuestore->get('tenantName'))->toBeNull();
+    expect($valuestore->get('tenantId'))->toBeNull()
+        ->and($valuestore->get('tenantName'))->toBeNull();
 
     $this->artisan('queue:work --once');
 
-    expect($this->valuestore->get('tenantId'))->toBe($this->tenant->getKey())
-        ->and($this->valuestore->get('tenantName'))->toBe($this->tenant->name);
+    expect($valuestore->get('tenantId'))->toBe($this->tenant->getKey())
+        ->and($valuestore->get('tenantName'))->toBe($this->tenant->name);
 });
