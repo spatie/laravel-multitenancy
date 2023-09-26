a00d27021599c6dd4e663722bfe6d503c283e325
Luca Longo <l.longo@ambita.it>
2023-09-26 12:26:53 +0200

    Update QueuedClosuresTest.php

--- tests/Feature/TenantAwareJobs/QueuedClosuresTest.php
+++ tests/Feature/TenantAwareJobs/QueuedClosuresTest.php
@@ -1,11 +1,15 @@
 <?php
 
+use Illuminate\Queue\CallQueuedClosure;
 use Spatie\Multitenancy\Models\Tenant;
 use Spatie\Valuestore\Valuestore;
 
 beforeEach(function () {
     config()->set('queue.default', 'database');
     config()->set('multitenancy.queues_are_tenant_aware_by_default', false);
+    config()->set('multitenancy.queueable_to_job', [
+        CallQueuedClosure::class => 'closure',
+    ]);
 
     $this->tenant = Tenant::factory()->create();
 });
@@ -20,18 +24,14 @@ it('succeeds with closure job when queues are tenant aware by default', function
     dispatch(function () use($valuestore) {
         $tenant = Tenant::current();
 
-        // $valuestore->put('tenantId', $tenant?->getKey());
-        // $valuestore->put('tenantName', $tenant?->name);
-        $valuestore->put('tenantName', 'test of mine');
+        $valuestore->put('tenantId', $tenant?->getKey());
+        $valuestore->put('tenantName', $tenant?->name);
     });
 
     $this->artisan('queue:work --once');
 
-    /*
     expect($valuestore->get('tenantId'))->toBe($this->tenant->getKey())
         ->and($valuestore->get('tenantName'))->toBe($this->tenant->name);
-    */
-    expect($valuestore->get('tenantName'))->toBe('test of mine');
 });
 
 it('fails with closure job when queues are not tenant aware by default', function () {
