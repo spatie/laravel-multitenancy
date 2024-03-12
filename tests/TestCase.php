fce1472b5d76ef3887111dea58e6b3993d6df1dc
Freek Van der Herten <freek@spatie.be>
2024-03-12 16:23:17 +0100

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,6 @@ use Spatie\Multitenancy\Tests\Feature\Commands\TestClasses\TenantNoopCommand;
 
 abstract class TestCase extends Orchestra
 {
-    use WithLaravelMigrations;
 
     protected function setUp(): void
     {
