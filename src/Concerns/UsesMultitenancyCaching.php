ccd2e63f1fac8fe8859b35d8ce8f6b8dec9b205b
François M <francoism90@users.noreply.github.com>
2024-06-25 08:40:39 +0200

    Update UsesMultitenancyCaching.php

--- src/Concerns/UsesMultitenancyCaching.php
+++ src/Concerns/UsesMultitenancyCaching.php
@@ -3,21 +3,52 @@
 namespace Spatie\Multitenancy\Concerns;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Spatie\Multitenancy\Exceptions\InvalidConfiguration;
 
 trait UsesMultitenancyCaching
 {
-    public function getTenantCacheKey(): string
+    protected function createTenantCache(): void
+    {        
+        $cacheStore = $this->getTenantCacheStore();
+        
+        if ($cacheStore === false) {
+            return;
+        }
+
+        $cacheKey = $this->getTenantCacheKey();
+
+        $cacheDuration = $this->getTenantCacheDuration();
+
+        if ($cacheDuration === null) {
+            cache()
+                ->store($cacheStore)
+                ->rememberForever($cacheKey, fn () => getTenantCollection());
+        }
+
+        cache()
+            ->store($cacheStore)
+            ->remember($cacheKey, $cacheDuration, fn () => getTenantCollection());
+    }
+
+    protected function getTenantCollection(): Collection
+    {
+        $tenantModelClass = config('multitenancy.tenant_model');
+
+        return app($tenantModelClass)::all();
+    }
+    
+    protected function getTenantCacheKey(): string
     {
         return config('multitenancy.cache_key', 'multitenancy'));
     }
 
-    public function getTenantCacheDuration(): ?int
+    protected function getTenantCacheDuration(): ?int
     {
         return config('multitenancy.cache_duration'));
     }
 
-    public function getTenantCacheStore(): mixed
+    protected function getTenantCacheStore(): mixed
     {
         return config('multitenancy.cache_store'));
     }
