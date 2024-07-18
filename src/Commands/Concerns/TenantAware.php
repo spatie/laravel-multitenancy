2911d53ef90a9108b1c57e79c1679d6eb21b3571
Luca Longo <l.longo@ambita.it>
2024-07-18 16:07:57 +0200

    wip

--- src/Commands/Concerns/TenantAware.php
+++ src/Commands/Concerns/TenantAware.php
@@ -30,7 +30,7 @@ trait TenantAware
 
         return $tenantQuery
             ->cursor()
-            ->map(fn ($tenant) => $tenant->execute(fn () => (int) $this->laravel->call([$this, 'handle'])))
+            ->map(fn (IsTenant $tenant) => $tenant->execute(fn () => (int) $this->laravel->call([$this, 'handle'])))
             ->sum();
     }
 }
